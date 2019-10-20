abb9306955fdcd3dded08ce41785c882ff88e765
Andrey <ag@infoservice.ru>
2019-10-20 17:10:09 +1000

    master Добавлены первичные настройки проекта

---
+++ public_html/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import 'materialize-css/dist/css/materialize.css';
+
+ReactDom.render(
+  <div>123456789</div>,
+document.getElementById('sbertest')
+);
